aaf3171f6c73c66863686f2cdbcb6957debfb21e
ClaytonCKOB <claytoncaco89@gmail.com>
2024-02-10 09:45:08 -0300

    feat: chat controller

---
+++ src/main/java/com/ckob/chat/dto/ChatMessage.java
@@ -0,0 +1,14 @@
+package com.ckob.chat.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+}
---
+++ src/main/java/com/ckob/chat/dto/MessageType.java
@@ -0,0 +1,7 @@
+package com.ckob.chat.dto;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
